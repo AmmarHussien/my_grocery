21a9e1e6d9cd5a3f6a9296e9affc585641a89fdb
ammarhussien <ammar.hussienmo97@gmail.com>
2023-05-07 13:43:28 +0300

    main header

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
+import '../../components/main_header.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return SafeArea(
+      child: Column(
+        children: const [
+          MainHeader(),
+        ],
+      ),
+    );
   }
 }
